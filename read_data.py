bcb564ca3b95302bbeff10f996385efe5e61451e
Bluhster <fjaptok@web.de>
2023-12-10 17:20:51 +0100

    try to include tags

--- read_data.py
+++ read_data.py
@@ -1,6 +1,6 @@
 import csv
 from sqlalchemy.exc import IntegrityError
-from models import Movie, MovieGenre
+from models import Movie, MovieGenre, MovieTags
 
 def check_and_read_data(db):
     # check if we have movies in the database
@@ -29,4 +29,28 @@ def check_and_read_data(db):
                 count += 1
                 if count % 100 == 0:
                     print(count, " movies read")
+
+        # read movies from csv
+        with open('data/tags.csv', newline='', encoding='utf8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            count = 0
+            for row in reader:
+                if count > 0:
+                    try:
+                        user_id = row[0]
+                        movie_id = row[1]
+                        timestamp = row[3]
+                        tags = row[2].split('|')  # genres is a list of genres
+                        for tag in tags:  # add each genre to the movie_genre table
+                            movie_tag = MovieTags(movie_id=movie_id, tag=tag)
+                            db.session.add(movie_tag)
+                        db.session.commit()  # save data to database
+                    except IntegrityError:
+                        print("Ignoring duplicate movie: " + title)
+                        db.session.rollback()
+                        pass
+                count += 1
+                if count % 100 == 0:
+                    print(count, " movies read")
+
 
