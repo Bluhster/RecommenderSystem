6c932a5a299b92ea31e4e0674495ec6516f4b189
ekuth <ekuth@uni-osnabrueck.de>
2024-01-02 15:02:40 +0100

    add possibility to rate

--- recommender.py
+++ recommender.py
@@ -1,6 +1,6 @@
 # Contains parts from: https://flask-user.readthedocs.io/en/latest/quickstart_app.html
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_user import login_required, UserManager
 import random
 
@@ -57,14 +57,13 @@ def movies_page():
     # first 10 movies
     # movies = Movie.query.limit(100).all()
 
-    all_genres = ["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama",
+    all_genres = ["Action", "Adventure", "Animation", "Children", "Comedy", "Crime", "Documentary", "Drama",
                   "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War",
                   "Western"]
     print("Movies Page")
 
     return render_template("filter_genre.html", all_genres=all_genres)
 
-    # return render_template("movies.html", movies=movies)
 
 
 @app.route('/filter_genre')
@@ -89,38 +88,57 @@ def filter_genre():
     return render_template("selected_genre.html", movies=movies)
 
 
-@app.route('/selected_genre', methods=['GET'])
+@app.route('/selected_genre', methods=['GET', 'POST'])
 @login_required
 def selected_genre():
+    if request.method == 'GET':
 
-    selected_genres = request.args.getlist('selectedGenres')
-    print("Received data:", selected_genres)
+        selected_genres = request.args.getlist('selectedGenres')
+        print("Received data:", selected_genres)
 
-    if selected_genres == 'No options selected.':
-        movies = []
-        # If no genres selected, show a random mix of 10 movies
-        all_genres = ["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama",
-                      "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War",
-                      "Western"]
-        random_genres = random.sample(all_genres, k=min(5, len(all_genres)))
-        for genre in random_genres:
-            genre_movies = Movie.query \
-                .filter(Movie.genres.any(MovieGenre.genre == genre)) \
-                .all()
-            movies.extend(genre_movies)
-        movies = random.sample(movies, k=min(10, len(movies)))
+        if selected_genres == 'No options selected.':
+            movies = []
+            # If no genres selected, show a random mix of 10 movies
+            all_genres = ["Action", "Adventure", "Animation", "Children", "Comedy", "Crime", "Documentary", "Drama",
+                          "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War",
+                          "Western"]
+            random_genres = random.sample(all_genres, k=min(5, len(all_genres)))
+            for genre in random_genres:
+                genre_movies = Movie.query \
+                    .filter(Movie.genres.any(MovieGenre.genre == genre)) \
+                    .all()
+                movies.extend(genre_movies)
+            movies = random.sample(movies, k=min(10, len(movies)))
+
+        else:
+            # print("SELECTED GENRES", selected_genres)
+            # Filter movies based on all selected genres
+            movies = Movie.query
+            for genre in selected_genres:
+                movies = movies.filter(Movie.genres.any(MovieGenre.genre == genre))
+
+            movies = movies.limit(15).all()
+
+        return render_template("selected_genre.html", movies=movies)
+    elif request.method == 'POST':
+        # Handle POST request for submitting ratings
+        data = request.form
+        movie_id = data.get('movieId')
+        rating = data.get('rating')
+        print("movie_id", movie_id)
+        print("Rating", rating)
+
+        # Save the rating to the database (replace with your logic)
+        # For example, you might have a Ratings table in your database
+        # where you can store the movie ID, user ID, and rating.
+
+        # Dummy response for illustration purposes
+        response_data = {'status': 'success', 'message': 'Rating submitted successfully'}
+        return jsonify(response_data)
 
     else:
-        # print("SELECTED GENRES", selected_genres)
-        # Filter movies based on all selected genres
-        movies = Movie.query
-        for genre in selected_genres:
-            movies = movies.filter(Movie.genres.any(MovieGenre.genre == genre))
-
-        movies = movies.limit(15).all()
-
-    return render_template("selected_genre.html", movies=movies)
-
+        # Handle other HTTP methods if needed
+        return jsonify({'status': 'error', 'message': 'Method not allowed'})
 
 
 # Start development web server
