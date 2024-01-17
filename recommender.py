c2d1c0beca23f6c92a49d98d3ea4435991d7593c
unknown <fjaptok@web.de>
2024-01-17 16:50:40 +0100

    added comment with console command possibly necessary for server deployment

--- recommender.py
+++ recommender.py
@@ -30,6 +30,8 @@ class ConfigClass(object):
     USER_AFTER_LOGIN_ENDPOINT = 'home_page'
     USER_AFTER_LOGOUT_ENDPOINT = 'home_page'
 
+    # use chown www-data movie_recommender.sqlite in console to make the database usable on server
+
 # Create Flask app
 app = Flask(__name__)
 app.config.from_object(__name__ + '.ConfigClass')  # configuration
